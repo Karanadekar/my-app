af812a39791838d675e9a8e782fb6772a39ecffe
Karanadekar <karanadekar10@gmail.com>
2024-01-08 22:02:32 +0530

    removed core-lib

--- src/pages/login-logout/login.jsx
+++ src/pages/login-logout/login.jsx
@@ -1,5 +1,5 @@
+import { Card, CardContent, Grid, Typography, ButtonBase, TextField } from "@mui/material";
 import React, { useState } from "react";
-// import Grid from 'material-ui-core/Grid';
 // import Card from 'material-ui-core/Card';
 // import CardBody from 'material-ui-core/CardContent';
 // import TextField from 'material-ui-core/TextField';
@@ -30,56 +30,58 @@ export default function Login() {
 
     return (
         <React.Fragment key={'Login Page'}>
-            <div style={{ height: '100vh', background: '#00000029', justifyContent: 'center' }}>
-                <div>
-                    <div style={{ borderRadius: '1rem' }}>
-                        <div>
+            <Grid container justifyContent={'center'} alignItems="center" style={{ height: '100vh' }}>
+                <Grid item xs={12} sm={8} md={6} lg={5}>
+                    <Card style={{ borderRadius: '1rem' }}>
+                        <CardContent>
                             <form onSubmit={handleSubmit}>
-                                <div direction="column" spacing={4} sx={{ borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-                                    <div>
-                                        <div variant="h4" component="h2" className="fw-bold mb-2 text-uppercase">
+                                <Grid container direction="column" alignItems="center" spacing={4}>
+                                    <Grid item>
+                                        <Typography variant="h4" component="h2" className="fw-bold mb-2 text-uppercase">
                                             {/* BuzzTalk🐝 */}
                                             My-app
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div variant="body1" className="text-white-50 mb-5">
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="body1" className="text-white-50 mb-5">
                                             Missing keys! Unlock the door with your Username and Password, please.
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <input
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <TextField
                                             type="text"
                                             name="username"
                                             value={formData.username}
                                             onChange={handleChange}
                                             label="Username"
                                             variant="outlined"
+                                            fullWidth
                                         />
-                                    </div>
-                                    <div>
-                                        <input
+                                    </Grid>
+                                    <Grid item>
+                                        <TextField
                                             type="password"
                                             name="password"
                                             value={formData.password}
                                             onChange={handleChange}
                                             label="Password"
                                             variant="outlined"
+                                            fullWidth
                                         />
-                                    </div>
-                                    <div>
-                                        <div variant="body2" className="small mb-5 pb-lg-2">
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="body2" className="small mb-5 pb-lg-2">
                                             <a href="#!" className="text-white-50">
                                                 Forgot password?
                                             </a>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <button variant="outlined" size="large" type="submit">
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <ButtonBase variant="outlined" size="large" type="submit">
                                             Login
-                                        </button>
-                                    </div>
-                                    <div>
+                                        </ButtonBase>
+                                    </Grid>
+                                    <Grid item>
                                         <div className="d-flex justify-content-center text-center mt-4 pt-1">
                                             <a href="#!" className="text-white">
                                                 <i className="fab fa-facebook-f fa-lg"></i>
@@ -91,21 +93,21 @@ export default function Login() {
                                                 <i className="fab fa-google fa-lg"></i>
                                             </a>
                                         </div>
-                                    </div>
-                                    <div>
-                                        <div variant="body2" className="mb-0">
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="body2" className="mb-0">
                                             Don't have an account?{' '}
-                                            <a href="/signup" className="text-white-50 fw-bold">
+                                            <a href="/signup" className="text-white-50 fw-bold" onClick={() => { }}>
                                                 Sign Up
                                             </a>
-                                        </div>
-                                    </div>
-                                </div>
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
                             </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </React.Fragment>
     )
 }
